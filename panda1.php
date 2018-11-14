42faf209c256f2fe46accfae3c96723840b670ea
JKmonca <45029387+JKmonca@users.noreply.github.com>
2018-11-14 17:41:38 +0900

    the first version
    with header (navbar, li, a href, divs)

---
+++ panda1.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>World Wildlife Foundation</title>
+    <link rel='stylesheet' href="./panda1.css" />
+</head>
+<body>
+    <div id="main_wrapper">
+        <div id="top_logo">
+        </div>
+        <header id="navbar">
+            <div class="menu_blue"> Our Work
+                <ul class="blue_underline"> 
+                    <li> <a href=""> Climate </a></li>
+                    <li> <a href=""> Food</a></li>
+                    <li> <a href=""> Forests</a></li>
+                    <li> <a href=""> Fresh Water</a></li>
+                    <li> <a href=""> Oceans </a></li>
+                    <li> <a href=""> Wildlife</a></li>
+                </ul>
+                <span class="link_arrow">
+                    <a href="">
+                    VIEW ALL CONSERVATION WORK >
+                    </a> 
+                </span>
+                <ul class="grey_background">
+                    <li> <a href="">Business</a></li>
+                    <li> <a href="">Policy</a></li>
+                    <li> <a href="">Partnerships</a></li>
+                    <li> <a href="">Science</a></li>
+                </ul>
+            </div>
+            
+            <div class="menu_blue"> Species
+                <ul class="blue_underline"> 
+                    <li> <a href=""> Giant Pandas </a></li>
+                    <li> <a href=""> Tigers </a></li>
+                    <li> <a href=""> Elephants </a></li>
+                    <li> <a href=""> Gorillas </a></li>
+                    <li> <a href=""> Sea Turtles </a></li>
+                    <li> <a href=""> Polar Bears</a></li>
+                    <li> <a href=""> Rhinos </a></li>
+                    <li> <a href=""> Whales </a></li>
+                </ul>
+                <span class="link_arrow">
+                    <a href=""> 
+                        VIEW SPECIES > 
+                    </a>
+                </span>
+                <ul class="grey_background">
+                    <li> <a href=""> Animal Trivia Games</a></li>
+                    <li> <a href=""> Symbolically Adopt an Animal</a></li>
+                    <li> <a href=""> Endangered Species </a></li>
+                    <li> <a href=""> Our Work Protecting Species </a></li>
+                </ul>
+            </div>
+            
+            <div class="menu_blue"> Places
+                <ul class="blue_underline"> 
+                    <li> <a href=""> Amazon</a></li>
+                    <li> <a href=""> Arctic </a></li>
+                    <li> <a href=""> Borneo and Sumatra</a></li> 
+                    <li> <a href=""> Congo Basin</a></li>
+                    <li> <a href=""> Coral Triangle </a></li>
+                    <li> <a href=""> Eastern Himalayas </a></li>
+                    <li> <a href=""> The Galapagos</a></li>
+                    <li> <a href=""> Northern Great Plains </a></li>
+                </ul>
+                <span class="link_arrow">
+                    <a href = " ">
+                    VIEW ALL WWF PRIORITY PLACES >
+                    </a>
+                </span>
+                <ul class="grey_background">
+                    <li> <a href=""> Travel with WWF </a></li>
+                    <li> <a href=""> Habitats </a></li>
+                    <li> <a href=""> Our Work Conserving Places </a></li>
+                </ul>
+            </div>
+            
+            <div class="menu_black"> ABOUT US
+                <ul class="black_background">
+                    <li> <a href=""> History </a></li>
+                    <li> <a href=""> Leadership </a></li>
+                    <li> <a href=""> Experts </a></li>
+                    <li> <a href=""> News & Press </a></li>
+                    <li> <a href=""> Financials </a></li>
+                    <li> <a href=""> Careers </a></li>
+                    <li> <a href=""> Contact </a></li>
+                </ul>
+            </div>
+            <div class="menu_black"> HOW TO HELP
+                <ul class="black_background">
+                    <li> <a href=""> Take Action </a></li>
+                    <li> <a href=""> Get Email </a></li>
+                    <li> <a href=""> Fundraise </a></li>
+                    <li> <a href=""> Send Ecards </a></li>
+                    <li> <a href=""> Shop to Support </a></li>
+                    <li> <a href=""> Travel with WWF </a></li>
+                    <li> <a href=""> See Other Ways to Support WWF </a></li>
+                </ul>
+            </div>
+
+            <div id="searchbar"> 
+
+            </div>
+
+            <div class="menu_black"> ADOPT
+                <ul class="black_background">
+                    <li> <a href=""> Adoptions </a></li>
+                    <li> <a href=""> Apparel </a></li>
+                    <li> <a href=""> More Gifts </a></li>
+                    <li> <a href=""> New for Holiday 2018 </a></li>
+                    <li> <a href=""> Snow Leopard Commercial </a></li>
+                </ul>
+            </div>
+
+            <div class="menu_black"> DONATE
+                <ul class="black_background">
+                    <li> <a href=""> Make a One-time Donation </a></li>
+                    <li> <a href=""> Make a Monthly Donation </a></li>
+                    <li> <a href=""> Become a Paperless Memeber </a></li>
+                    <li> <a href=""> Renew Your Membership </a></li>
+                    <li> <a href=""> Join as a Partner in Conservation </a></li>
+                </ul>
+            </div>
+
+        </header>
+        
+
+
+
+
+
+
+
+
+
+    <footer id="footer">
+
+    </footer>
+
+    </div>
+
+<script>
+</script>
+
+</body>
+</html>
