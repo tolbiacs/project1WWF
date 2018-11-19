56e5e83ac21f4a71635f63450af1c0cf93e164d0
kjunpark92 <45029422+kjunpark92@users.noreply.github.com>
2018-11-19 18:32:00 +0900

    Add files via upload

--- panda_header.php
+++ panda_header.php
@@ -1,69 +1,74 @@
-<!-- <div class="menu_blue"> Our Work
-    <ul class="blue_underline"> 
-        <li> <a href="https://www.worldwildlife.org/initiatives/climate"> Climate </a></li>
-        <li> <a href="https://www.worldwildlife.org/initiatives/food"> Food</a></li>
-        <li> <a href="https://www.worldwildlife.org/initiatives/forests"> Forests</a></li>
-        <li> <a href="https://www.worldwildlife.org/initiatives/fresh-water"> Fresh Water</a></li>
-        <li> <a href="https://www.worldwildlife.org/initiatives/oceans"> Oceans </a></li>
-        <li> <a href="https://www.worldwildlife.org/initiatives/wildlife-conservation"> Wildlife</a></li>
-    </ul>
-    <span class="link_arrow">
-        <a href="https://www.worldwildlife.org/initiatives">
-        VIEW ALL CONSERVATION WORK >
-        </a> 
-    </span>
-    <ul class="grey_background">
-        <li> <a href="https://www.worldwildlife.org/initiatives/transforming-business">Business</a></li>
-        <li> <a href="https://www.worldwildlife.org/initiatives/influencing-policy">Policy</a></li>
-        <li> <a href="https://www.worldwildlife.org/pages/partnerships">Partnerships</a></li>
-        <li> <a href="https://www.worldwildlife.org/initiatives/science">Science</a></li>
-    </ul>
-</div>
-<div class="menu_blue"> Species
-    <ul class="blue_underline"> 
-        <li> <a href="https://www.worldwildlife.org/species/giant-panda"> Giant Pandas </a></li>
-        <li> <a href="https://www.worldwildlife.org/species/tiger"> Tigers </a></li>
-        <li> <a href="https://www.worldwildlife.org/species/elephant"> Elephants </a></li>
-        <li> <a href="https://www.worldwildlife.org/species/gorilla"> Gorillas </a></li>
-        <li> <a href="https://www.worldwildlife.org/species/sea-turtle"> Sea Turtles </a></li>
-        <li> <a href="https://www.worldwildlife.org/species/polar-bear"> Polar Bears</a></li>
-        <li> <a href="https://www.worldwildlife.org/species/rhino"> Rhinos </a></li>
-        <li> <a href="https://www.worldwildlife.org/species/whale"> Whales </a></li>
-    </ul>
-    <span class="link_arrow">
-        <a href="https://www.worldwildlife.org/species"> 
-            VIEW SPECIES > 
-        </a>
-    </span>
-    <ul class="grey_background">
-        <li> <a href="https://www.worldwildlife.org/pages/animal-trivia-games"> Animal Trivia Games</a></li>
-        <li> <a href="https://gifts.worldwildlife.org/gift-center/Default.aspx?sc=AWY1904OQ19068A02866RX"> Symbolically Adopt an Animal</a></li>
-        <li> <a href="https://www.worldwildlife.org/species/directory?sort=extinction_status&direction=desc"> Endangered Species </a></li>
-        <li> <a href="https://www.worldwildlife.org/teaching-resources/"> Our Work Protecting Species </a></li>
-    </ul>
-</div>
-<div class="menu_blue"> Places
-    <ul class="blue_underline"> 
-        <li> <a href="https://www.worldwildlife.org/places/amazon"> Amazon</a></li>
-        <li> <a href="https://www.worldwildlife.org/places/arctic"> Arctic </a></li>
-        <li> <a href="https://www.worldwildlife.org/places/borneo-and-sumatra"> Borneo and Sumatra</a></li> 
-        <li> <a href="https://www.worldwildlife.org/places/congo-basin"> Congo Basin</a></li>
-        <li> <a href="https://www.worldwildlife.org/places/coral-triangle"> Coral Triangle </a></li>
-        <li> <a href="https://www.worldwildlife.org/places/eastern-himalayas"> Eastern Himalayas </a></li>
-        <li> <a href="https://www.worldwildlife.org/places/the-galapagos"> The Galapagos</a></li>
-        <li> <a href="https://www.worldwildlife.org/places/northern-great-plains"> Northern Great Plains </a></li>
-    </ul>
-    <span class="link_arrow">
-        <a href = "https://www.worldwildlife.org/places">
-        VIEW ALL WWF PRIORITY PLACES >
-        </a>
-    </span>
-    <ul class="grey_background">
-        <li> <a href="https://www.worldwildlife.org/travel"> Travel with WWF </a></li>
-        <li> <a href="https://www.worldwildlife.org/habitats"> Habitats </a></li>
-        <li> <a href="https://www.worldwildlife.org/initiatives/conserving-places"> Our Work Conserving Places </a></li>
-    </ul>
-</div> -->
+<!-- <ul class="menu_blue"> 
+    <li class="top_category"> <span> Our Work </span> 
+        <div class="left_list">
+            <ul class="blue_underline"> 
+                <li> <a href="https://www.worldwildlife.org/initiatives/climate"> Climate </a></li>
+                <li> <a href="https://www.worldwildlife.org/initiatives/food"> Food</a></li>
+                <li> <a href="https://www.worldwildlife.org/initiatives/forests"> Forests</a></li>
+                <li> <a href="https://www.worldwildlife.org/initiatives/fresh-water"> Fresh Water</a></li>
+                <li> <a href="https://www.worldwildlife.org/initiatives/oceans"> Oceans </a></li>
+                <li> <a href="https://www.worldwildlife.org/initiatives/wildlife-conservation"> Wildlife</a></li>
+            </ul>
+            <span class="link_arrow">
+                <a href="https://www.worldwildlife.org/initiatives">
+                VIEW ALL CONSERVATION WORK >
+                </a> 
+            </span>
+        </div>
+        <ul class="grey_background">
+            <li> <a href="https://www.worldwildlife.org/initiatives/transforming-business">Business</a></li>
+            <li> <a href="https://www.worldwildlife.org/initiatives/influencing-policy">Policy</a></li>
+            <li> <a href="https://www.worldwildlife.org/pages/partnerships">Partnerships</a></li>
+            <li> <a href="https://www.worldwildlife.org/initiatives/science">Science</a></li>
+        </ul>
+    </li>
+    <li class="top_category"> <span> Species </span> 
+        <div class="left_list">
+            <ul class="blue_underline"> 
+                <li> <a href="https://www.worldwildlife.org/species/giant-panda"> Giant Pandas </a></li>
+                <li> <a href="https://www.worldwildlife.org/species/tiger"> Tigers </a></li>
+                <li> <a href="https://www.worldwildlife.org/species/elephant"> Elephants </a></li>
+                <li> <a href="https://www.worldwildlife.org/species/gorilla"> Gorillas </a></li>
+                <li> <a href="https://www.worldwildlife.org/species/sea-turtle"> Sea Turtles </a></li>
+                <li> <a href="https://www.worldwildlife.org/species/polar-bear"> Polar Bears</a></li>
+                <li> <a href="https://www.worldwildlife.org/species/rhino"> Rhinos </a></li>
+                <li> <a href="https://www.worldwildlife.org/species/whale"> Whales </a></li>
+            </ul> 
+            <span class="link_arrow"><a href="https://www.worldwildlife.org/species"> 
+VIEW SPECIES ></a></span>
+        </div>
+            <ul class="grey_background">
+                <li> <a href="https://www.worldwildlife.org/pages/animal-trivia-games"> Animal Trivia Games</a></li>
+                <li> <a href="https://gifts.worldwildlife.org/gift-center/Default.aspx?sc=AWY1904OQ19068A02866RX"> Symbolically Adopt an Animal</a></li>
+                <li> <a href="https://www.worldwildlife.org/species/directory?sort=extinction_status&direction=desc"> Endangered Species </a></li>
+                <li> <a href="https://www.worldwildlife.org/teaching-resources/"> Our Work Protecting Species </a></li>
+            </ul>
+    </li>
+    <li class="top_category" > <span> Places</span>
+        <div class="left_list">
+            <ul class="blue_underline"> 
+                <li> <a href="https://www.worldwildlife.org/places/amazon"> Amazon</a></li>
+                <li> <a href="https://www.worldwildlife.org/places/arctic"> Arctic </a></li>
+                <li> <a href="https://www.worldwildlife.org/places/borneo-and-sumatra"> Borneo and Sumatra</a></li> 
+                <li> <a href="https://www.worldwildlife.org/places/congo-basin"> Congo Basin</a></li>
+                <li> <a href="https://www.worldwildlife.org/places/coral-triangle"> Coral Triangle </a></li>
+                <li> <a href="https://www.worldwildlife.org/places/eastern-himalayas"> Eastern Himalayas </a></li>
+                <li> <a href="https://www.worldwildlife.org/places/the-galapagos"> The Galapagos</a></li>
+                <li> <a href="https://www.worldwildlife.org/places/northern-great-plains"> Northern Great Plains </a></li>
+            </ul>
+            <span class="link_arrow">
+                <a href = "https://www.worldwildlife.org/places">
+                VIEW ALL WWF PRIORITY PLACES >
+                </a>
+            </span>
+        </div>
+            <ul class="grey_background">
+                <li> <a href="https://www.worldwildlife.org/travel"> Travel with WWF </a></li>
+                <li> <a href="https://www.worldwildlife.org/habitats"> Habitats </a></li>
+                <li> <a href="https://www.worldwildlife.org/initiatives/conserving-places"> Our Work Conserving Places </a></li>
+            </ul>
+    </li>
+</ul> -->
 <div class="menu_black" id="about_us"><a href="https://www.worldwildlife.org/about"> ABOUT US </a>
     <ul class="black_background">
         <li><a href="https://www.worldwildlife.org/about/history"><span> History </span></a></li>
