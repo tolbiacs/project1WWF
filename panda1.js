5070b91bb56cdf829f4dbc95ef5267175d34ff90
kjunpark92 <45029422+kjunpark92@users.noreply.github.com>
2018-11-21 06:42:22 +0900

    Add files via upload

---
+++ panda1.js
@@ -0,0 +1,15 @@
+// var ten_dollars = document.querySelector("#ten_dollars");
+
+// ten_dollars.addEventListener('click',function(e){
+//     e.target.style.backgroundColor='red';
+// });
+// (function () {
+//     var select_amount = document.querySelectorAll(".select_amount");
+//     console.log(select_amount);
+//     for (var i=0; i<select_amount.length; i++){
+//         select_amount[i].addEventListener('click', function(e){
+            
+//             e.target.style.backgroundColor="red";
+//         });
+//     }
+// } ());
